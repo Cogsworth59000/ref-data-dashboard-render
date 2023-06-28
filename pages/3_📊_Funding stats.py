fb565c637f77e8292936f1369f80ed9313aeb9d0
Thomas Palmer <thmsplmr23@gmail.com>
2023-06-28 16:41:54 +0100

    New commit 28 Jun 23 16.41

--- pages/3_📊_Funding stats.py
+++ pages/3_📊_Funding stats.py
@@ -342,12 +342,12 @@ data={
     }
 )
 
-styler_vol = volume_table.style.hide().format(subset=[
+styler_vol = volume_table.style.hide_index().format(subset=[
     "REF 2014","REF 2021", "Factor of"],
     decimal='.',
     precision=2)
 
-styler_fund = funding_table.style.hide().format(subset=[
+styler_fund = funding_table.style.hide_index().format(subset=[
     "REF 2014","REF 2021", "Factor of"],
     decimal='.',
     precision=2)
